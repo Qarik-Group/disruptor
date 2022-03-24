c15fe2aa6bcd7243706e8e0307a99e3fb4751c70
Patryk Kozak <thehackbee@gmail.com>
2022-03-24 09:50:34 +0100

    Add unstable channel as nixpkgs_latest

---
+++ scripts/nixpkgs-latest.nix
@@ -0,0 +1,12 @@
+{ system ? builtins.currentSystem, ... }@args:
+
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ../flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ../.; }
+).defaultNix.outputs.nixpkgs_latest.${system}
