48233892933aa080285f02da1432fa0240b37e66
Patryk Kozak <thehackbee@gmail.com>
2022-03-24 16:22:30 +0100

    Add common-module used in other module

--- projects/bzl5_bzlmod/java_cmd/JavaCmd.java
+++ projects/bzl5_bzlmod/java_cmd/JavaCmd.java
@@ -1,9 +1,13 @@
 package com.qarik.disruptor;
 
+import com.qarik.disruptor.common.AsciiBanner;
+
 class JavaCmd
 {
 	public static void main (String[] args) throws java.lang.Exception
-	{
+	{	
+		AsciiBanner banner = new AsciiBanner("JavaCmd");
+		banner.show();
 		System.out.println("Hello!");
 	}
 }
